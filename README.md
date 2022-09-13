34bd88eedf9d43e4d50617432fc84ea00b9f999c
Kathy <66495979+kathy678@users.noreply.github.com>
2022-09-13 15:58:11 -0400

    merge

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 Creating a new branch is quick and simple.
 Git is a free software.
 new branch (dev)
+this is a test
---
+++ hello.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import types
+
+
+class Animal(object):   #编写Animal类
+    def run(self):
+        print("Animal is running...")
+
+class Dog(Animal):  #Dog类继承Amimal类，没有run方法
+    pass
+
+class Cat(Animal):  #Cat类继承Animal类，有自己的run方法
+    def run(self):
+        print('Cat is running...')
+    pass
+
+class Car(object):  #Car类不继承，有自己的run方法
+    def run(self):
+        print('Car is running...')
+
+class Stone(object):  #Stone类不继承，也没有run方法
+    pass
+
+def run_twice(arg):
+	arg.run()
+	arg.run()
+
+
+class Student(object):
+    count = 0
+
+    def __init__(self, name):
+        self.name = name
+        Student.count += 1
+
+
+class FooError(ValueError):
+    pass
+
+def foo(s):
+    n = int(s)
+    if n==0:
+        raise FooError('invalid value: %s' % s)
+    return 10 / n
+
+foo('0')
